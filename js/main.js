332b3a7096c3e9bc0a4bc8e41f0c6b44c10cef33
paul0127 <paul@eso.com.tw>
2021-06-30 18:47:00 +0800

    product

--- js/main.js
+++ js/main.js
@@ -34,20 +34,54 @@ window.addEventListener('click', (event) => {
 window.addEventListener('resize', () => {
   if (window.innerWidth > 1450) {
     body.classList.remove('fixed')
-  }else if(window.innerWidth <= 1450 && search_block.classList.contains('active')){
+  } else if (
+    window.innerWidth <= 1450 &&
+    search_block.classList.contains('active')
+  ) {
     body.classList.add('fixed')
   }
 })
 
-menu_btn.addEventListener('click',()=>{
+menu_btn.addEventListener('click', () => {
   menu.classList.toggle('active')
 })
 
 let arrow_down = document.querySelector('.index_top .arrow_down')
-arrow_down.addEventListener('click', () => {
-  let index_about_top = document.querySelector('.index_about').offsetTop
-  window.scrollTo({
-    top: index_about_top,
-    behavior: 'smooth',
+if (arrow_down)
+  arrow_down.addEventListener('click', () => {
+    let index_about_top = document.querySelector('.index_about').offsetTop
+    window.scrollTo({
+      top: index_about_top,
+      behavior: 'smooth',
+    })
   })
-})
+
+let left_menu = document.querySelector('.product_frame .left_menu')
+if (left_menu) {
+  let left_menu_items = left_menu.querySelectorAll(
+    '.left_menu_border > ul > li'
+  )
+
+  left_menu_items.forEach((item) => {
+    let left_menu_item = item.querySelector('ul')
+    if (left_menu_item) {
+      let height = left_menu_item.offsetHeight
+
+      if (item.classList.contains('active')) {
+        left_menu_item.style.height = height + 'px'
+      } else {
+        left_menu_item.style.height = '0px'
+      }
+
+      let left_menu_items_btn = item.querySelector('.name')
+      left_menu_items_btn.addEventListener('click', () => {
+        left_menu_items_btn.parentElement.classList.toggle('active')
+        if (left_menu_items_btn.parentElement.classList.contains('active')) {
+          left_menu_item.style.height = height + 'px'
+        } else {
+          left_menu_item.style.height = '0px'
+        }
+      })
+    }
+  })
+}
